2387877927db5ffd48ea27cb8fbd773db9418429
8EAUTIFUL <pawel.wardachpr@gmail.com>
2023-11-10 16:55:24 +0100

    Added tests and fix for pytest

---
+++ images/tests1/test_basic_services.py
@@ -0,0 +1,105 @@
+import pytest
+import tempfile
+from PIL import Image
+from django.contrib.auth import get_user_model
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.http import Http404
+from rest_framework.test import APIClient
+from ..services.basic_services import get_image_details, get_user_images, delete_image, create_image_obj
+from ..models import UserImage, UserProfile, AccountTier
+from images.services.validation_services import validate_image_format
+from images.tasks import resize_image
+
+User = get_user_model()
+
+
+@pytest.fixture
+def create_basic_acc_tier():
+    basic_acc_tier = AccountTier.objects.create(
+        name='Basic',
+        original_image_link=False,
+        expiring_link=False,
+        thumbnail_height=200,
+        thumbnail_width=1
+    )
+    return basic_acc_tier
+
+
+@pytest.fixture
+def create_premium_acc_tier():
+    basic_acc_tier = AccountTier.objects.create(
+        name='Premium',
+        original_image_link=True,
+        expiring_link=False,
+        thumbnail_height=400,
+        thumbnail_width=1
+    )
+    return basic_acc_tier
+
+
+@pytest.fixture
+def create_enterprise_acc_tier():
+    basic_acc_tier = AccountTier.objects.create(
+        name='Enterprise',
+        original_image_link=True,
+        expiring_link=True,
+        thumbnail_height=400,
+        thumbnail_width=1
+    )
+    return basic_acc_tier
+
+
+@pytest.fixture
+def create_authenticated_user_with_user_profile_instance(create_basic_acc_tier):
+    """"The built-in Django User model is automatically extended with a UserProfile through signals in models.py."""
+    user = User.objects.create_user(username='123', password='123')
+    client = APIClient()
+    client.login(username='123', password='123')
+    return user, client
+
+
+@pytest.fixture
+def create_user_image(create_authenticated_user_with_user_profile_instance):
+    user, client = create_authenticated_user_with_user_profile_instance
+
+    image_path = 'images/tests1/test_images/test_img.jpg'
+    image = Image.open(image_path)
+
+    tmp_file = tempfile.NamedTemporaryFile(suffix=".jpg")
+    image.save(tmp_file.name)
+    uploaded_file = SimpleUploadedFile("test_image.jpg", tmp_file.read(), content_type="image/jpg")
+    user_image = UserImage.objects.create(user=user.userprofile, image=uploaded_file)
+    return user_image
+
+
+@pytest.mark.django_db
+class TestUserImageLogic:
+    def test_get_image_details(self, create_user_image):
+        user_image = create_user_image
+        result_image = get_image_details(image_id=user_image.id, user=user_image.user)
+
+        assert user_image == result_image
+        assert UserImage.objects.count() == 1
+        assert UserProfile.objects.count() == 1
+        assert AccountTier.objects.count() == 1
+
+    def test_get_image_details_invalid_data(self, create_user_image):
+        user_image = create_user_image
+        with pytest.raises(Http404):
+            get_image_details(image_id=99999999, user=user_image.user)
+
+    def test_get_user_images(self, create_user_image):
+        user_image = create_user_image
+        result_images = get_user_images(user=user_image.user)
+
+        assert len(result_images) == 1
+        assert user_image in result_images
+
+    def test_delete_user_image(self, create_user_image):
+        user_image = create_user_image
+        delete_image(image_id=user_image.pk, user=user_image.user)
+
+        assert not UserImage.objects.filter(pk=user_image.pk).exists()
+
+    def test_create_image_object(self, mocker, create_user_image):
+        user_image = create_user_image
