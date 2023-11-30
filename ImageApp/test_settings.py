9f49c0b09fe2e380dc300268677ea1630849267e
BEAUTIFULUU <pawel.wardachpr@gmail.com>
2023-11-30 20:32:48 +0100

    CI fix

--- ImageApp/test_settings.py
+++ ImageApp/test_settings.py
@@ -4,7 +4,7 @@ from .settings import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'test_db',
+        'NAME': 'test_tb',
         'USER': 'test_user',
         'PASSWORD': 'test_password',
         'HOST': 'db',
